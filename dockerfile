7120060016d7d66c66a45aceb0f82c6d0d27b2ee
pAkalpa <pasinduakalpa1998@gmail.com>
2023-09-25 18:18:22 +0530

    🎨 Just Update

--- dockerfile
+++ dockerfile
@@ -16,6 +16,9 @@ WORKDIR /app
 COPY --from=deps /app/node_modules ./node_modules
 COPY . .
 ENV NODE_ENV production
+ENV NEXT_PUBLIC_SUPABASE_URL https://npwrarexrluofnntconu.supabase.co
+ENV SUPABASE_SERVICE_ROLE_KEY eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wd3JhcmV4cmx1b2ZubnRjb251Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NTYxNTE4OCwiZXhwIjoyMDExMTkxMTg4fQ.af57g_vIFHB5ayeyz1VAkk9q-R8oPCj4HXe_b10AUuM
+ENV NEXT_PUBLIC_SUPABASE_ANON_KEY eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wd3JhcmV4cmx1b2ZubnRjb251Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2MTUxODgsImV4cCI6MjAxMTE5MTE4OH0.zhiQ4kUTtxx3uNOF4Q4N9rlUZjEnOSRlu5OwQoAkFnU
 RUN npx next telemetry disable
 RUN npm run build
 
@@ -29,5 +32,6 @@ COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
 COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
 USER nextjs
 EXPOSE 3000
+ENV HOSTNAME 127.0.0.1
 ENV PORT 3000
 CMD ["node", "server.js"]
--- selfhosting/setup-dump.sql
+++ selfhosting/setup-dump.sql
@@ -1,26 +1,32 @@
 
 -- Table Definition
-CREATE TABLE IF NOT EXISTS "public"."profile" (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+CREATE TABLE
+IF NOT EXISTS "public"."profile"
+(
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4
+(),
     "name" text,
     "email" text,
     "plan" text NOT NULL DEFAULT 'free',
     "updated_at" timestamptz,
-    PRIMARY KEY ("id")
+    PRIMARY KEY
+("id")
 );
 
 -- Table Definition
-CREATE TABLE "public"."app" (
+CREATE TABLE "public"."app"
+(
     "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "created_at" timestamptz DEFAULT now(),
-    "owner" uuid  NOT NULL,
+    "owner" uuid NOT NULL,
     "name" text NOT NULL,
     PRIMARY KEY ("id")
 );
 
 
 -- Table Definition
-CREATE TABLE "public"."log" (
+CREATE TABLE "public"."log"
+(
     "id" int8 NOT NULL,
     "created_at" timestamptz DEFAULT now(),
     "message" text,
@@ -32,7 +38,8 @@ CREATE TABLE "public"."log" (
 );
 
 -- Table Definition
-CREATE TABLE "public"."run" (
+CREATE TABLE "public"."run"
+(
     "created_at" timestamptz DEFAULT now(),
     "tags" _text,
     "app" uuid,
@@ -94,27 +101,35 @@ ALTER TABLE public.run ENABLE ROW LEVEL SECURITY;
 -- Name: profile Profiles are viewable by users who created them.; Type: POLICY; Schema: public; Owner: -
 --
 
-CREATE POLICY "Profiles are viewable by users who created them." ON public.profile FOR SELECT USING ((auth.uid() = id));
+CREATE POLICY "Profiles are viewable by users who created them." ON public.profile FOR
+SELECT USING ((auth.uid() = id));
 
 
 --
 -- Name: profile Users can insert their own profile.; Type: POLICY; Schema: public; Owner: -
 --
 
-CREATE POLICY "Users can insert their own profile." ON public.profile FOR INSERT WITH CHECK ((auth.uid() = id));
+CREATE POLICY "Users can insert their own profile." ON public.profile FOR
+INSERT WITH CHECK ((
+auth.uid()
+= id));
 
 --
 -- Name: profile Users can update own profile.; Type: POLICY; Schema: public; Owner: -
 --
 
-CREATE POLICY "Users can update own profile." ON public.profile FOR UPDATE USING ((auth.uid() = id));
+CREATE POLICY "Users can update own profile." ON public.profile FOR
+UPDATE USING ((auth.uid()
+= id));
 
 
 --
 -- Name: app app_owner_policy; Type: POLICY; Schema: public; Owner: -
 --
 
-CREATE POLICY app_owner_policy ON public.app USING (auth.uid() = app.owner);
+CREATE POLICY app_owner_policy ON public.app USING
+(auth.uid
+() = app.owner);
 
 
 
@@ -122,18 +137,24 @@ CREATE POLICY app_owner_policy ON public.app USING (auth.uid() = app.owner);
 -- Name: log log_owner_policy; Type: POLICY; Schema: public; Owner: -
 --
 
-CREATE POLICY log_owner_policy ON public.log USING ((( SELECT app.owner
-   FROM public.app
-  WHERE (app.id = log.app)) = auth.uid()));
+CREATE POLICY log_owner_policy ON public.log USING
+((( SELECT app.owner
+FROM public.app
+WHERE (app.id = log.app))
+= auth.uid
+()));
 
 
 --
 -- Name: run run_owner_policy; Type: POLICY; Schema: public; Owner: -
 --
 
-CREATE POLICY run_owner_policy ON public.run USING ((( SELECT app.owner
-   FROM public.app
-  WHERE (app.id = run.app)) = auth.uid()));
+CREATE POLICY run_owner_policy ON public.run USING
+((( SELECT app.owner
+FROM public.app
+WHERE (app.id = run.app))
+= auth.uid
+()));
 
 
 
@@ -141,36 +162,45 @@ CREATE POLICY run_owner_policy ON public.run USING ((( SELECT app.owner
 -- Name: run run_owner_policy; Type: POLICY; Schema: public; Owner: -
 --
 
-CREATE POLICY app_user_owner_policy ON public.app_user USING ((( SELECT app.owner
-   FROM public.app
-  WHERE (app.id = app_user.app)) = auth.uid()));
+CREATE POLICY app_user_owner_policy ON public.app_user USING
+((( SELECT app.owner
+FROM public.app
+WHERE (app.id = app_user.app))
+= auth.uid
+()));
 
 
 --
 -- Name: get_related_runs(uuid); Type: FUNCTION; Schema: public; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION public.get_related_runs(run_id uuid)
- RETURNS TABLE(created_at timestamp with time zone, tags text[], app uuid, id uuid, status text, name text, ended_at timestamp with time zone, error jsonb, input jsonb, output jsonb, params jsonb, type text, parent_run uuid, completion_tokens integer, prompt_tokens integer)
+CREATE OR REPLACE FUNCTION public.get_related_runs
+(run_id uuid)
+ RETURNS TABLE
+(created_at timestamp
+with time zone, tags text[], app uuid, id uuid, status text, name text, ended_at timestamp
+with time zone, error jsonb, input jsonb, output jsonb, params jsonb, type text, parent_run uuid, completion_tokens integer, prompt_tokens integer)
  LANGUAGE plpgsql
 AS $function$
 BEGIN
     RETURN QUERY
-    WITH RECURSIVE related_runs AS (
-        SELECT r1.*
+    WITH RECURSIVE related_runs AS
+            (
+                SELECT r1.*
         FROM run r1
         WHERE r1.id = run_id
 
-        UNION ALL
+    UNION ALL
 
         SELECT r2.*
         FROM run r2
-        INNER JOIN related_runs rr ON rr.id = r2.parent_run
+            INNER JOIN related_runs rr ON rr.id = r2.parent_run
     )
-    SELECT rr.created_at, rr.tags, rr.app, rr.id, rr.status, rr.name, rr.ended_at, rr.error, rr.input, rr.output, 
-           rr.params, rr.type, rr.parent_run, rr.completion_tokens, rr.prompt_tokens
+    SELECT rr.created_at, rr.tags, rr.app, rr.id, rr.status, rr.name, rr.ended_at, rr.error, rr.input, rr.output,
+        rr.params, rr.type, rr.parent_run, rr.completion_tokens, rr.prompt_tokens
     FROM related_runs rr;
-END; $function$
+END;
+$function$
 
 
 
@@ -178,37 +208,43 @@ END; $function$
 -- Name: get_runs_usage(uuid, integer); Type: FUNCTION; Schema: public; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION public.get_runs_usage(app_id uuid, days integer, user_id bigint DEFAULT NULL::bigint)
- RETURNS TABLE(name text, type text, completion_tokens bigint, prompt_tokens bigint, errors bigint, success bigint)
+CREATE OR REPLACE FUNCTION public.get_runs_usage
+(app_id uuid, days integer, user_id bigint DEFAULT NULL::bigint)
+ RETURNS TABLE
+(name text, type text, completion_tokens bigint, prompt_tokens bigint, errors bigint, success bigint)
  LANGUAGE plpgsql
 AS $function$
 BEGIN
-    RETURN QUERY 
-    SELECT 
+    RETURN QUERY
+    SELECT
         run.name,
         run.type,
         COALESCE(SUM(run.completion_tokens), 0) AS completion_tokens,
         COALESCE(SUM(run.prompt_tokens), 0) AS prompt_tokens,
         SUM(CASE WHEN run.status = 'error' THEN 1 ELSE 0 END) AS errors,
         SUM(CASE WHEN run.status = 'success' THEN 1 ELSE 0 END) AS success
-    FROM 
+    FROM
         run
     WHERE 
         run.app = app_id AND
-        run.created_at >= NOW() - INTERVAL '1 day' * days AND
-        (user_id IS NULL OR run.user = user_id)  AND
-    	(run.type != 'agent' OR run.parent_run IS NULL)
-GROUP BY
-        run.name, run.type;
+        run.created_at >= NOW() - INTERVAL
+    '1 day' * days AND
+    (user_id IS NULL OR run.user = user_id)  AND
+    (run.type != 'agent' OR run.parent_run IS NULL)
+    GROUP BY
+            run.name, run.type;
+END; $function$
 
 
-CREATE OR REPLACE FUNCTION public.get_runs_usage_daily(app_id uuid, days integer, user_id integer DEFAULT NULL)
- RETURNS TABLE(date date, name text, type text, completion_tokens bigint, prompt_tokens bigint, errors bigint, success bigint)
+CREATE OR REPLACE FUNCTION public.get_runs_usage_daily
+(app_id uuid, days integer, user_id integer DEFAULT NULL)
+ RETURNS TABLE
+(date date, name text, type text, completion_tokens bigint, prompt_tokens bigint, errors bigint, success bigint)
  LANGUAGE plpgsql
 AS $function$
 BEGIN
-    RETURN QUERY 
-    SELECT 
+    RETURN QUERY
+    SELECT
         DATE(run.created_at) AS date,
         run.name,
         run.type,
@@ -216,24 +252,27 @@ BEGIN
         COALESCE(SUM(run.prompt_tokens), 0) AS prompt_tokens,
         SUM(CASE WHEN run.status = 'error' THEN 1 ELSE 0 END) AS errors,
         SUM(CASE WHEN run.status = 'success' THEN 1 ELSE 0 END) AS success
-    FROM 
+    FROM
         run
     WHERE 
         run.app = app_id AND
-        run.created_at >= NOW() - INTERVAL '1 day' * days AND
-        (user_id IS NULL OR run.user = user_id) AND
-    	(run.type != 'agent' OR run.parent_run IS NULL)
+        run.created_at >= NOW() - INTERVAL
+    '1 day' * days AND
+    (user_id IS NULL OR run.user = user_id) AND
+    (run.type != 'agent' OR run.parent_run IS NULL)
     GROUP BY
         date, run.name, run.type;
 END; $function$
 
-CREATE OR REPLACE FUNCTION public.get_runs_usage_by_user(app_id uuid, days integer)
- RETURNS TABLE(user_id bigint, name text, type text, completion_tokens bigint, prompt_tokens bigint, errors bigint, success bigint)
+CREATE OR REPLACE FUNCTION public.get_runs_usage_by_user
+(app_id uuid, days integer)
+ RETURNS TABLE
+(user_id bigint, name text, type text, completion_tokens bigint, prompt_tokens bigint, errors bigint, success bigint)
  LANGUAGE plpgsql
 AS $function$
 BEGIN
-    RETURN QUERY 
-    SELECT 
+    RETURN QUERY
+    SELECT
         run.user as user_id,
         run.name,
         run.type,
@@ -241,21 +280,23 @@ BEGIN
         COALESCE(SUM(run.prompt_tokens), 0) AS prompt_tokens,
         SUM(CASE WHEN run.status = 'error' THEN 1 ELSE 0 END) AS errors,
         SUM(CASE WHEN run.status = 'success' THEN 1 ELSE 0 END) AS success
-    FROM 
+    FROM
         run
     WHERE 
         run.app = app_id AND
-        run.created_at >= NOW() - INTERVAL '1 day' * days AND
-    	(run.type != 'agent' OR run.parent_run IS NULL)
+        run.created_at >= NOW() - INTERVAL
+    '1 day' * days AND
+    (run.type != 'agent' OR run.parent_run IS NULL)
     GROUP BY
         user_id, run.name, run.type;
 END; $function$
-    
+
 
 
 --
 -- Name: idx_run_parent_run; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE INDEX idx_run_parent_run ON public.run USING btree (parent_run);
+CREATE INDEX idx_run_parent_run ON public.run USING btree
+(parent_run);
 
